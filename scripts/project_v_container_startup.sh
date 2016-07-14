f11062f3904bbe60f18662cec325fceacae25051
Andre Senna <andre.senna@gmail.com>
2016-07-14 14:11:29 -0300

    .

--- scripts/project_v_container_startup.sh
+++ scripts/project_v_container_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export RELEX_CONTAINER_IP=$(cat /etc/hosts | grep relex | cut -f1 | sort -u)
-guile -c ../opencog/nlp/chatbot/run-chatbot.scm
+guile -l ../opencog/nlp/chatbot/run-chatbot.scm
 while true; do
 sleep 1
 done
